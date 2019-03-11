8efb62f9f9689cc8ff46fb4af84fb49b9b428dc9
andrzej.plutecki <andrzej.plutecki@j-labs.pl>
2019-03-11 07:46:11 +0100

    fix importance of sort

--- src/main/scala/indexing/Sort/SortStrategy.scala
+++ src/main/scala/indexing/Sort/SortStrategy.scala
@@ -6,11 +6,11 @@ object SortStrategy {
   private def Desc[T: Ordering] = implicitly[Ordering[T]].reverse
 
   def sortByMatchedWordsAndNumberOfOccurrence(results: Map[String, SearchResult]) = {
-    results.toSeq.sortBy(d => (d._2.sumOfWordFound, d._2.numberOfWordsFound))(Desc).toMap
+    results.toSeq.sortBy(d => (d._2.numberOfWordsFound, d._2.sumOfWordFound))(Desc).toMap
   }
 
   def sortByMatchedWordsAndWordsInRow(results: Map[String, SearchResult]) = {
-    results.toSeq.sortBy(d => (d._2.sumOfWordFound, d._2.wordsInRow))(Desc).toMap
+    results.toSeq.sortBy(d => (d._2.wordsInRow, d._2.sumOfWordFound))(Desc).toMap
   }
 
   def sortByMatchedWords(results: Map[String, SearchResult]) = {
